892bc62e8208c048ccac43de5a38ab1d79a1c41f
Jeremy Ebler <jebler@gmail.com>
2015-01-11 00:18:49 -0800

    Google Chart

--- backups.rb
+++ backups.rb
@@ -11,20 +11,32 @@ stores = Dir['backups/*/backupstore.txt']
 rebuild_index = proc do
   puts "Rebuilding index..."
   start = Time.now
+
   backups = []
+  backup_chart = []
+  total_bytes = 0
+
   stores.each do |backupstore|
     IO.readlines(backupstore).reverse.each do |backup|
       parts = backup.chomp!.split '='
 
       file = "backups/#{parts[0..3].join '/'}/Backup-#{parts[0..3].join '-'}--#{parts[4..5].join '-'}.zip"
+      bytes = File.size file
 
       backups << {
         name: "#{parts[0]} #{Time.new *parts[1..5]}",
         url: "/#{file}",
-        size: Filesize.from("#{File.size file} B").to_s('MiB'),
+        size: Filesize.from("#{bytes} B").to_s('MiB'),
       }
+
+      backup_chart.unshift ["#{Time.new *parts[1..5]}", bytes]
+
+      total_bytes += bytes
     end
   end
+  backup_chart.unshift %w(Date Bytes)
+  backup_chart = JSON.generate backup_chart
+  total_bytes = Filesize.from("#{total_bytes} B").to_s('MiB')
 
   template = ERB.new File.read("index.html.erb")
   File.write 'index.html', template.result(binding)
